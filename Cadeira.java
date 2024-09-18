40a86f8f2aa354581dec08fac7aa5cce61bdc523
amarallorena <159277845+amarallorena@users.noreply.github.com>
2024-09-18 15:30:19 -0300

    cadeira

---
+++ Cadeira.java
@@ -0,0 +1,54 @@
+package Cadeira;
+
+public class Cadeira {
+		String marca;
+		int rodinhas;
+		String cor;
+		int braços;
+		String roda;
+		String inclina;
+		String anda;
+		public String getMarca() {
+		return marca;
+		}
+		public void setMarca(String marca) {
+		this.marca = marca;
+		}
+		public String getCor() {
+		return cor;
+		}
+		public void setCor(String cor) {
+		this.cor = cor;
+		}
+		public int Rodinhas() {
+		return rodinhas;
+		}
+		public void setRodinhas(int rodinhas) {
+		this.rodinhas = rodinhas;
+		}
+		public int getBraços() {
+		return braços;
+		}
+		public void setBraços(int braços) {
+		this.braços = braços;
+		}
+		public String getRoda() {
+		return roda;
+		}
+		public void setRoda(String roda) {
+		this.roda = roda;
+		}
+		public String getInclina() {
+		return inclina;
+		}
+		public void setInclina(String inclina) {
+		this.inclina = inclina;
+		}
+		public String getAnda() {
+		return anda;
+		}
+		public void setAnda(String anda) {
+		this.anda = anda;
+
+		}
+}	
